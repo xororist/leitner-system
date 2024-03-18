ea6e900c7c891ad3481cd532d790d510af51adf3
xororist <hcachon@myges.fr>
2024-03-18 23:45:55 +0100

    Update DependencyInjection.cs

--- LeitnerSystem.Infrastructure/DependencyInjection.cs
+++ LeitnerSystem.Infrastructure/DependencyInjection.cs
@@ -3,10 +3,8 @@ using LeitnerSystem.Infrastructure.Repositories;
 using Microsoft.Extensions.DependencyInjection;
 using MongoDB.Bson.Serialization;
 using LeitnerSystem.Domain.Entities;
-using LeitnerSystem.Domain.ValueObjects;
 using LeitnerSystem.Infrastructure.MongoDatabase;
-using LeitnerSystem.Infrastructure.Utils;
-using MongoDB.Bson.Serialization.IdGenerators;
+
 
 namespace LeitnerSystem.Infrastructure;
 
@@ -27,31 +25,11 @@ public static class DependencyInjection
     {
         BsonClassMap.RegisterClassMap<Card>(cm =>
         {
-            cm.AutoMap();
-            cm.MapIdMember(c => c.Id).SetIdGenerator(GuidGenerator.Instance);
-            cm.MapMember(c => c.Question).SetSerializer(new ValueObjectSerializer<Question>());
-            cm.MapMember(c => c.Answer).SetSerializer(new ValueObjectSerializer<Answer>());
-            cm.MapMember(c => c.Metadata).SetSerializer(new ValueObjectSerializer<Metadata>());
-        });
-
-        BsonClassMap.RegisterClassMap<Question>(cm =>
-        {
-            cm.AutoMap();
-            cm.MapMember(q => q.Text).SetElementName("question");
-        });
-
-        BsonClassMap.RegisterClassMap<Answer>(cm =>
-        {
-            cm.AutoMap();
-            cm.MapMember(a => a.Text).SetElementName("answer");
-        });
-
-        BsonClassMap.RegisterClassMap<Metadata>(cm =>
-        {
-            cm.AutoMap();
-            cm.MapMember(m => m.CreationDate).SetElementName("creationDate");
-            cm.MapMember(m => m.NextDateQuestion).SetElementName("nextDateQuestion");
-            cm.MapMember(m => m.IsCompleted).SetElementName("isCompleted");
+            cm.AutoMap(); 
+            cm.MapMember(c => c.Question); 
+            cm.MapMember(c => c.Answer); 
+            cm.MapMember(c => c.Metadata); 
         });
     }
+
 }
