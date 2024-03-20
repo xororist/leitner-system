1b3e9d4953df209585b172eb215db929d6bd14e7
hugo-cachon <hcachon@myges.fr>
2024-03-20 22:57:29 +0100

    Required annotations to create or update an answer

--- backend/LeitnerSystem.Application/Dto/CreateCardDto.cs
+++ backend/LeitnerSystem.Application/Dto/CreateCardDto.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LeitnerSystem.Application.Dto;
 
 public class CreateCardDto
 {
+    [Required]
     public string Question { get; set; }
+    [Required]
     public string Answer { get; set; }
     public string Tag { get; set; }
 }
--- backend/LeitnerSystem.Application/Dto/UpdateCardDto.cs
+++ backend/LeitnerSystem.Application/Dto/UpdateCardDto.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LeitnerSystem.Application.Dto;
 
 public class UpdateCardDto
 {
     public Guid CardId { get; set; }
+    [Required]
     public string Question { get; set; }
+    [Required]
     public string Answer { get; set; }
     public string Tag { get; set; }
 }
